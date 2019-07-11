ddf5354d6528adbfb219e7d5dd1963966e4ab44d
thangnd85 <thangnd85@gmail.com>
2019-07-11 21:12:16 +0700

    Up buster

--- get-docker.sh
+++ get-docker.sh
@@ -292,7 +292,7 @@ do_install() {
 			dist_version="$(sed 's/\/.*//' /etc/debian_version | sed 's/\..*//')"
 			case "$dist_version" in
 				10)
-					dist_version="stretch"
+					dist_version="buster"
 				;;
 				9)
 					dist_version="stretch"
